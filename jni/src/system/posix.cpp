bad7ce8114dbc5a2db4f59e181f0a0304ad5b1a3
shangzebei <917058965@qq.com>
2017-12-20 17:47:33 +0800

    aaa

--- jni/src/system/posix.cpp
+++ jni/src/system/posix.cpp
@@ -865,7 +865,7 @@ class MySystem : public System {
 
   virtual Status load(System::Library** lib, const char* name)
   {
-    fprintf(stdout,"dlopen %s \n",name);
+    fprintf(stdout,"dlopen %s \n",BOOTSTRAP_PROPERTY);
     unsigned nameLength = (name ? strlen(name) : 0);
     bool isMain = name == 0;
     if (isMain) {
